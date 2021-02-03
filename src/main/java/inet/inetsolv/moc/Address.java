b6a1d285af39f27a6e29606a87d6e1e58c15eead
Sumanth Talasila <sumanth.inetsolv@gmail.com>
2021-02-03 08:45:05 +0530

    JIRA-124. Address.java

--- src/main/java/inet/inetsolv/moc/Address.java
+++ src/main/java/inet/inetsolv/moc/Address.java
@@ -3,7 +3,7 @@ package inet.inetsolv.moc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Person {
+public class Address {
 
 
 }
