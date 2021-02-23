5762b11aa78d69452f883a3ac2be37ed4df4cf18
Sumanth Talasila <sumanth.inetsolv@gmail.com>
2021-02-23 08:48:28 +0530

    Adding demo.java

---
+++ src/main/java/inet/inetsolv/moc/Phones.java
@@ -0,0 +1,9 @@
+package inet.inetsolv.moc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Phones {
+
+
+}
